c647d18a288caa68c53721349e045f550b098418
sheri94 <88737393+sheri94@users.noreply.github.com>
2021-10-10 20:43:50 +0500

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,48 +1,36 @@
-import New.Roma;
-
-import java.sql.SQLOutput;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
 
 public class calculator {
-    public static void main(String[] args ) {
-
-        System.out.println("Введите все символы через пробел и нажмите Enter");
-            Scanner in = new Scanner(System.in);
-            String text = in.nextLine();
-            Pattern pattern = Pattern.compile("[ ]");
-            String[] strings = pattern.split(text);
-            int a = Integer.parseInt(strings[0]);
-            char z = strings[1].charAt(0);
-            int b = Integer.parseInt(strings[2]);
-
-       Roma roma = Roma.valueOf(strings[0]);
-       Roma roma1 =Roma.valueOf(strings[2]);
-
-
-
-        if(a>10||b >10) {
-                System.exit(0); // если число больше 10 то останавливает программу
-            }
-       /* System.out.println(a);
-        System.out.println(z);
-        System.out.println(b);*/
-        if(z=='+') {
-                System.out.println(a + b);}
-        else if(z=='-') {
-                System.out.println(a - b);}
-        else if(z=='*') {
-                System.out.println(a * b);}
-        else if(z=='/') {
-                System.out.println(a / b);}
-        else {
-                System.exit(0);}
-        }
+    public calculator() {
     }
 
+    public static void main(String[] args) {
+        System.out.println("Введите все символы через пробел и нажмите Enter");
+        Scanner in = new Scanner(System.in);
+        String text = in.nextLine();
+        Pattern pattern = Pattern.compile("[ ]");
+        String[] strings = pattern.split(text);
+        int a = Integer.parseInt(strings[0]);
+        char z = strings[1].charAt(0);
+        int b = Integer.parseInt(strings[2]);
+
+       
+
+        if (a > 10 || b > 10) {
+            System.exit(0);
+        }
+
+        if (z == '+') {
+            System.out.println(a + b);
+        } else if (z == '-') {
+            System.out.println(a - b);
+        } else if (z == '*') {
+            System.out.println(a * b);
+        } else if (z == '/') {
+            System.out.println(a / b);
+        } else {
+            System.exit(0);
+        }
+
+    }
